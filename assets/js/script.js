2725c24f34defc057212ad63600d135c2e223ed1
Cawaale256 <abdi.awale5@gmail.com>
2024-08-17 16:56:56 +0000

    Add js questions and answer options

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const question = [
+{
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    answer: "Paris"
+},
+{
+    question : "What is the capital of Spain?",
+    options : ["Paris", "London", "Berlin", "Madrid"],
+    answer : "Madrid"
+},
+{
+    question : "What is the capital of Germany?",
+    options : ["Paris", "London", "Berlin", "Madrid"],
+    answer : "Berlin"
+},
+{
+    question : "What is the capital of England?",
+    options : ["Paris", "London", "Berlin", "Madrid"],
+    answer : "London"
+}
+] 
+
+
